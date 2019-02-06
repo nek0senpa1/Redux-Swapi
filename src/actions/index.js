eda65d8795ff7b414429f5c991987235b52b33b2
amberm <nekosenpai@gmail.com>
2019-02-06 14:22:44 -0700

    Got A few new redux-thunk things working I think

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,3 +6,33 @@
 // our action creator will be a function that returns a function
 // the url to fetch characters from is `https://swapi.co/api/people/`
 // remember that now we have controll over our thunk-based action creator
+
+
+import axios from 'axios';
+
+export const GET_SW_START = 'GET_SW_START';
+export const GET_SW_SUCCESS = 'GET_SW_SUCCESS';
+export const GET_SW_FAIL = 'GET_SW_FAIL';
+
+export const getStarWars = () => dispatch => {
+    dispatch ({
+        type: GET_SW_START
+    });
+    axios
+    .get ('https://swapi.co/api/people')
+    .then (res => {
+        console.log(res)
+        dispatch({
+            type: GET_SW_SUCCESS,
+            payload: res.data
+        })
+    })
+    .catch(err => {
+        console.log(err)
+        dispatch ({
+            type: GET_SW_FAIL,
+            payload: "The Force Was Not Strong With You..."
+        })
+    })
+
+}
